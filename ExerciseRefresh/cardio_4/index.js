aaff2fbffaa85acc2b9240edbe4e1ce50b22b6ea
alouzao10 <alouzao10@gmail.com>
2021-03-01 15:53:12 -0500

    working on some Promises practice

--- ExerciseRefresh/cardio_4/index.js
+++ ExerciseRefresh/cardio_4/index.js
@@ -54,4 +54,58 @@ function countSheep(num) {
 console.log(countSheep(1));
 console.log(countSheep(2));
 console.log(countSheep(3));
-console.log(countSheep(15));
+//console.log(countSheep(15));
+
+console.log('+++++++++++++++++++++++++++++++++++++');
+
+// Arrow Functions
+let helloArrow = () => {
+  console.log('Hello Arrow Function');
+};
+helloArrow();
+
+let countArray = (arr) => {
+  return arr.reduce((result, num) => {
+    return result + num;
+  }, 0);
+};
+
+console.log(countArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+console.log('+++++++++++++++++++++++++++++++++++++');
+
+// Working with Promises
+let holdOn = (sec) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (sec > 10000) {
+        reject('Waited too long');
+      } else {
+        resolve('The time has passed');
+      }
+    }, sec);
+  });
+};
+
+let doSomething = async () => {
+  console.log("Let's wait 5 seconds");
+  let response = await holdOn(5000);
+  console.log(response);
+  console.log('All done');
+  let somePromise = new Promise((resolve, reject) => {
+    if ((5000 / 1000) % 5 === 0) {
+      resolve('Waited Correctly');
+    } else {
+      reject('Waited Incorrectly');
+    }
+  });
+  somePromise
+    .then((ret) => {
+      console.log(ret);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+doSomething();
